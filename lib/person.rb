436327318f8f6353a36602d4ed0a0b085ddf3432
Zach <zachlandis91@gmail.com>
2023-04-11 12:50:30 -0600

    commit tree d6757e07f16e4bf9bd6b877fb5f2732c0fede3bd

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,79 @@
-# your code goes here
+class Person
+
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+    
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(number)
+        @happiness = if number > 10
+                        10
+                    elsif number < 0
+                        0
+                    else 
+                        number
+                    end
+    end
+
+    def hygiene=(number)
+        @hygiene = if number > 10
+                        10
+                elsif number < 0
+                        0
+                else
+                    number
+                end
+    end
+
+    def happy?
+        self.happiness > 7 
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end    
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+
+
+end
